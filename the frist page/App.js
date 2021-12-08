1233afa7ff4a682bbef74d8e32b19745f8eeed11
mostafa1112000 <92263439+mostafa1112000@users.noreply.github.com>
2021-12-08 08:39:48 +0200

    Add files via upload

---
+++ the frist page/App.js
@@ -0,0 +1,13 @@
+import react, {component} from 'react'
+
+export default class App extends component{
+    render(){
+        return(
+
+            
+            <div>
+            <h1>Mostafa mahmoud said</h1>
+            </div>
+        )
+    }
+}
---
+++ the frist page/index.js
@@ -0,0 +1,6 @@
+import reactDom from "reactDom"
+import App from "./App"
+
+
+
+reactDom,render(<App/> ,document,getElementByid("root"))
